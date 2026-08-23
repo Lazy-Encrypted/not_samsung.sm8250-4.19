azzlantern/kernel_xiaomi_sm8250

exec: Use strstr() to relieve multiple expensive strcmp() calls Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

include/linux/binfmts.h
@@ -158,16 +158,10 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
-	       !strcmp(comm, "power@1.3-servi") ||
-	       !strcmp(comm, "perf@1.0-servic") ||
-	       !strcmp(comm, "perf@2.0-servic") ||
-	       !strcmp(comm, "perf@2.1-servic") ||
-	       !strcmp(comm, "perf@2.2-servic") ||
-	       !strcmp(comm, "power@2.0-servic") ||
-               !strcmp(comm, "power-service.") ||
-	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	return strstr(comm, "init")  || strstr(comm, "NodeLooperThread") ||
+	       strstr(comm, "power") ||
+	       strstr(comm, "perf")  ||
+	       strstr(comm, "iop");
 }
 
 #endif /* _LINUX_BINFMTS_H */

Lazy-Encrypted/not_samsung.sm8250-4.19

Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

include/linux/binfmts.h
@@ -157,16 +157,10 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
-	       !strcmp(comm, "power@1.3-servi") ||
-	       !strcmp(comm, "perf@1.0-servic") ||
-	       !strcmp(comm, "perf@2.0-servic") ||
-	       !strcmp(comm, "perf@2.1-servic") ||
-	       !strcmp(comm, "perf@2.2-servic") ||
-	       !strcmp(comm, "power@2.0-servic") ||
-               !strcmp(comm, "power-service.") ||
-	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	return strstr(comm, "init")  || strstr(comm, "NodeLooperThrea") ||
+	       strstr(comm, "power") ||
+	       strstr(comm, "perf")  ||
+	       strstr(comm, "iop");
 }
 
 #endif /* _LINUX_BINFMTS_H */