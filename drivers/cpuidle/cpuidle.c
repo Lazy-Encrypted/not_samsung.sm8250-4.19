0e0d1469c43be808abbf071bf939fa15fccc49ef
Lazy <adriantachyon69@gmail.com>
2026-09-05 14:09:15 +0000

    cpuidle: inline ktime diff to fix declaration after statement

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -254,8 +254,7 @@ int __nocfi cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_drive
 	if (!cpuidle_state_is_coupled(drv, index))
 		local_irq_enable();
 
-	ktime_t diff = ktime_sub(time_end, time_start);
-
+		dev->last_residency_ns = (int) ktime_sub(time_end, time_start);
 	dev->last_residency_ns = (int) diff;
 
 	if (entered_state >= 0) {
