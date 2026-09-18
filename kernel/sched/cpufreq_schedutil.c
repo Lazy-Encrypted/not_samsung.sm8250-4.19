2b440cfdb772c4d51287b7856b3a63559a935cf2
Lazy <adriantachyon69@gmail.com>
2026-09-18 04:37:28 +0000

    kernel: sched: set schedutil rate_limit_us to 1000 for faster response at CPU freq bump

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -949,7 +949,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 
-	tunables->rate_limit_us = 2000;
+	tunables->rate_limit_us = 1000;
 	tunables->response_time_ms = sugov_calc_freq_response_ms(sg_policy);
 	sugov_update_response_time_mult(sg_policy);
 	sugov_build_dvfs_headroom_lut(sg_policy);
